e9ca5a082d6c259ea2576699baa48553661fdfba
Oliver Tappe <zooey@hirschkaefer.de>
2003-09-13 17:21:41 +0000

    -	added chartables.h as part of compilation cleanup.

---
+++ src-bmRegexx/chartables.h
@@ -0,0 +1,13 @@
+/*************************************************
+*      Perl-Compatible Regular Expressions       *
+*************************************************/
+
+/* This file is automatically written by the dftables auxiliary 
+program. If you edit it by hand, you might like to edit the Makefile to 
+prevent its ever being regenerated.
+
+This file is #included in the compilation of pcre.c to build the default
+character tables which are used when no tables are passed to the compile
+function. */
+
+extern unsigned char pcre_default_tables[];
