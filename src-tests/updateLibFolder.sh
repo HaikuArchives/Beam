2e5bec67a6a07e33652ee7426c5cd188d74ea61b
Oliver Tappe <zooey@hirschkaefer.de>
2003-09-23 13:16:01 +0000

    - added this file to setup TestBeam-environment

---
+++ src-tests/updateLibFolder.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+# create link to libs-subfolder of devel-branch:
+ln -sf ../build/devel/lib lib
+
+# create link to add-ons-subfolder of devel-branch:
+ln -sf ../build/devel/add-ons add-ons
+
+# create link to Deskbar-item of devel-branch:
+ln -sf ../build/devel/Beam_DeskbarItem Beam_DeskbarItem
