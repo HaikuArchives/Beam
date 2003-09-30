096f4aaae6f75ac6f70e67a392c63a3c578825d4
Oliver Tappe <zooey@hirschkaefer.de>
2003-09-30 18:46:08 +0000

    -	added some more tests for problematic encoded-word encoding.

--- src-tests/QuotedPrintableEncoderTest.cpp
+++ src-tests/QuotedPrintableEncoderTest.cpp
@@ -136,8 +136,8 @@ QuotedPrintableEncoderTest::SimpleTest() {
 		"=E0=E1=E2=E3=E4=E5=E6=E7=E8=E9=EA=EB=EC=ED=EE=EF",
 		"=F0=F1=F2=F3=F4=F5=F6=F7=F8=F9=FA=FB=FC=FD=FE=FF"
 	};
-	NextSubTest(); 
 	for( int i = 0; i<16; ++i) {
+		NextSubTest(); 
 		EncodeQpAndCheck( AsciiAlphabet[i], result[i]);
 	}
 }
@@ -173,8 +173,9 @@ QuotedPrintableEncoderTest::EncodedWordTest() {
 	NextSubTest(); 
 	EncodeQpAndCheck( "Simple encoding =C3=9F",
 							"=?utf-8?q?Simple_encoding_=3DC3=3D9F?=");
-	// complete alphabet test, 16 lines with 16 chars (minus 1):
-	BmString result[16] = {
+	// lower half alphabet test, 8 lines with 16 chars (minus 1):
+	// (upper half left out because it would yield illegal utf-8 characters)
+	BmString result[8] = {
 		"=?utf-8?q?=01=02=03=04=05=06=07=08=09=0A=0B=0C=0E=0F?=",
 							// yes, =0D (\r) is dumped!
 		"=?utf-8?q?=10=11=12=13=14=15=16=17=18=19=1A=1B=1C=1D=1E=1F?=",
@@ -184,18 +185,11 @@ QuotedPrintableEncoderTest::EncodedWordTest() {
 		"=?utf-8?q?PQRSTUVWXYZ[\\]^=5F?=",
 		"=?utf-8?q?=60abcdefghijklmno?=",
 		"=?utf-8?q?pqrstuvwxyz{|}~=7F?=",
-		"=?utf-8?q?=80=81=82=83=84=85=86=87=88=89=8A=8B=8C=8D=8E=8F?=",
-		"=?utf-8?q?=90=91=92=93=94=95=96=97=98=99=9A=9B=9C=9D=9E=9F?=",
-		"=?utf-8?q?=A0=A1=A2=A3=A4=A5=A6=A7=A8=A9=AA=AB=AC=AD=AE=AF?=",
-		"=?utf-8?q?=B0=B1=B2=B3=B4=B5=B6=B7=B8=B9=BA=BB=BC=BD=BE=BF?=",
-		"=?utf-8?q?=C0=C1=C2=C3=C4=C5=C6=C7=C8=C9=CA=CB=CC=CD=CE=CF?=",
-		"=?utf-8?q?=D0=D1=D2=D3=D4=D5=D6=D7=D8=D9=DA=DB=DC=DD=DE=DF?=",
-		"=?utf-8?q?=E0=E1=E2=E3=E4=E5=E6=E7=E8=E9=EA=EB=EC=ED=EE=EF?=",
-		"=?utf-8?q?=F0=F1=F2=F3=F4=F5=F6=F7=F8=F9=FA=FB=FC=FD=FE=FF?="
 	};
-	NextSubTest(); 
-	for( int i = 0; i<16; ++i)
+	for( int i = 0; i<8; ++i) {
+		NextSubTest(); 
 		EncodeQpAndCheck( AsciiAlphabet[i], result[i]);
+	}
 }
 
 /*------------------------------------------------------------------------------*\
@@ -419,8 +413,8 @@ QuotedPrintableEncoderTest::MultiLineEncodedWordTest() {
 		"12345678901234567890123456789012345678901234567890123456789ä12345678",
 		//
 		"=?utf-8?q?1234567890123456789012345678901234567890123456789012345678"
-		"9=C3?=\r\n"
-		" =?utf-8?q?=A412345678?="
+		"9?=\r\n"
+		" =?utf-8?q?=C3=A412345678?="
 	);
 	// check encoding near wrapping border (doesn't fit on current line):
 	NextSubTest(); 
@@ -428,8 +422,8 @@ QuotedPrintableEncoderTest::MultiLineEncodedWordTest() {
 		"123456789012345678901234567890123456789012345678901234567890ä2345678",
 		//
 		"=?utf-8?q?1234567890123456789012345678901234567890123456789012345678"
-		"90=C3?=\r\n"
-		" =?utf-8?q?=A42345678?="
+		"90?=\r\n"
+		" =?utf-8?q?=C3=A42345678?="
 	);
 	// check encoding near wrapping border (doesn't fit on current line):
 	NextSubTest(); 
@@ -437,8 +431,8 @@ QuotedPrintableEncoderTest::MultiLineEncodedWordTest() {
 		"1234567890123456789012345678901234567890123456789012345678901ä345678",
 		//
 		"=?utf-8?q?1234567890123456789012345678901234567890123456789012345678"
-		"901=C3?=\r\n"
-		" =?utf-8?q?=A4345678?="
+		"901?=\r\n"
+		" =?utf-8?q?=C3=A4345678?="
 	);
 	// check encoding near wrapping border (doesn't fit on current line):
 	NextSubTest(); 
@@ -454,9 +448,9 @@ QuotedPrintableEncoderTest::MultiLineEncodedWordTest() {
 	NextSubTest(); 
 	EncodeQpAndCheck( "Re: ありがとうございます。 (and more...)",
 							"=?utf-8?q?Re:_=E3=81=82=E3=82=8A=E3=81=8C=E3=81=A8=E3"
-								"=81=86=E3=81=94=E3=81?=\r\n"
-							" =?utf-8?q?=96=E3=81=84=E3=81=BE=E3=81=99=E3=80=82"
-								"_(and_more...)?=");
+								"=81=86=E3=81=94?=\r\n"
+							" =?utf-8?q?=E3=81=96=E3=81=84=E3=81=BE=E3=81=99=E3=80"
+								"=82_(and_more...)?=");
 }
 
 /*------------------------------------------------------------------------------*\
