b8c638cc67b173a29856b78daea2d80f251b66dc
Oliver Tappe <zooey@hirschkaefer.de>
2004-06-25 14:21:33 +0000

    -	fixed compilation issue under Dano.

--- src-bmGuiBase/BmDividable.cpp
+++ src-bmGuiBase/BmDividable.cpp
@@ -30,6 +30,15 @@
 
 #include <cstdarg>
 
+#include <BeBuild.h>
+#ifdef B_BEOS_VERSION_DANO
+	class BFont;
+	class BMessage;
+	class BMessageRunner;
+	class BRect;
+	class BStatusBar;
+#endif
+
 #include <layout.h>
 
 #include "BmDividable.h"
