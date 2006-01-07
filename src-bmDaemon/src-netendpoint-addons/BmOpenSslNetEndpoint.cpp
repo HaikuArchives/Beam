cc08f6c9abcb3e12604768b03e8badeefc8bbed0
Oliver Tappe <zooey@hirschkaefer.de>
2006-01-07 23:19:12 +0000

    -	created addon that implements encryption via openssl. Making 	this an addon keeps Beam itself free of dependencies to 	openssl (which may not be installed on every system). Using 	the addon, Beam will be able to use encryption on systems 	that have openssl installed and fall back to no encryption 	on systems where openssl isn't available. -	the actual openssl-code has kindly been provided by 	Ingo Weinhold, thanks a lot!

---
+++ src-bmDaemon/src-netendpoint-addons/BmOpenSslNetEndpoint.cpp
@@ -0,0 +1,328 @@
+/*
+ * Copyright 2005, Ingo Weinhold, bonefish@cs.tu-berlin.de.
+ * Distributed under the terms of the MIT license.
+ *
+ * Changed into an addon and extended somewhat 
+ * by Oliver Tappe, zooey@hirschkaefer.de.
+ */
+
+#include <Message.h>
+#include <NetEndpoint.h>
+
+#include "BmOpenSslNetEndpoint.h"
+
+#include <openssl/ssl.h>
+#include <openssl/err.h>
+
+// Automatically initialize libopenssl.
+struct _InitOpenSSL {
+	_InitOpenSSL()
+	{
+		SSL_library_init();
+		SSL_load_error_strings();
+	}
+};
+static _InitOpenSSL sInitOpenSSL;
+
+/*------------------------------------------------------------------------------*\
+	BmOpenSslNetEndpoint()
+		-	constructor
+\*------------------------------------------------------------------------------*/
+BmOpenSslNetEndpoint::BmOpenSslNetEndpoint()
+	:	mSSL(NULL)
+	,	mError(0)
+{
+}
+
+/*------------------------------------------------------------------------------*\
+	~BmOpenSslNetEndpoint()
+		-	destructor
+\*------------------------------------------------------------------------------*/
+BmOpenSslNetEndpoint::~BmOpenSslNetEndpoint() 
+{
+	// cleanup error queue from current thread's TLS:
+	ERR_remove_state(0);
+}
+
+/*------------------------------------------------------------------------------*\
+	Connect()
+		-	
+\*------------------------------------------------------------------------------*/
+status_t BmOpenSslNetEndpoint::Connect( const BNetAddress& address) {
+	// already connected?
+	if (mSSL)
+		return B_BAD_VALUE;
+
+	// establish the underlying socket connection
+	status_t error = inherited::Connect( address);
+	if (error != B_OK)
+		return error;
+
+	return _StartEncryptionAfterConnecting();
+}
+
+/*------------------------------------------------------------------------------*\
+	Close()
+		-	
+\*------------------------------------------------------------------------------*/
+void BmOpenSslNetEndpoint::Close() {
+	StopEncryption();
+	inherited::Close();
+}
+
+/*------------------------------------------------------------------------------*\
+	Error()
+		-	
+\*------------------------------------------------------------------------------*/
+int BmOpenSslNetEndpoint::Error() const
+{
+	if (mError != 0)
+		return mError;
+	else
+		return inherited::Error();
+}
+
+/*------------------------------------------------------------------------------*\
+	ErrorStr()
+		-	
+\*------------------------------------------------------------------------------*/
+BmString BmOpenSslNetEndpoint::ErrorStr() const
+{
+	if (mError != 0) {
+		BmString errStr;
+		uint32 err;
+		char errBuf[128];
+		while((err = ERR_get_error()) != 0) {
+			ERR_error_string(err, errBuf);
+			errStr << errBuf << "\n";
+		}
+		return errStr;
+	} else
+		return inherited::ErrorStr();
+}
+
+/*------------------------------------------------------------------------------*\
+	StartEncryption()
+		-	
+\*------------------------------------------------------------------------------*/
+void BmOpenSslNetEndpoint::SetEncryptionType(const char* encType)
+{
+	mEncryptionType = encType;
+}
+
+/*------------------------------------------------------------------------------*\
+	StartEncryption()
+		-	
+\*------------------------------------------------------------------------------*/
+status_t BmOpenSslNetEndpoint::StartEncryption(const char* encType) {
+	// encryption already on?
+	if (mSSL)
+		return B_NOT_ALLOWED;
+
+	// connected?
+	int socket = mSocket->Socket();
+	if (socket < 0)
+		return ENOTCONN;
+
+	if (mEncryptionType != encType)
+		mEncryptionType = encType;
+
+	// check type
+	SSL_METHOD* method;
+	if (mEncryptionType == "TLS")
+		method = TLSv1_client_method();
+	else if (mEncryptionType == "SSL")
+		method = SSLv3_client_method();
+	else
+		return B_BAD_VALUE;
+
+	// create SSL context
+	SSL_CTX* context = SSL_CTX_new( method);
+	if (!context)
+		return B_ERROR;
+
+	// create SSL structure
+	ssl_st* ssl = SSL_new( context);
+	if (!ssl) {
+		SSL_CTX_free( context);
+		return B_ERROR;
+	}
+
+	// free the context (decrement its ref count); the SSL structure holds a
+	// reference now
+	SSL_CTX_free( context);
+
+	// set SSL file descriptor
+	if (SSL_set_fd( ssl, socket) != 1) {
+		SSL_free( ssl);
+		return B_ERROR;
+	}
+
+	// connect
+	int result = SSL_connect( ssl);
+	if (result != 1) {
+		mError = SSL_get_error( mSSL, result);
+		SSL_free( ssl);
+		return B_ERROR;
+	}
+
+	// Everything went fine; keep the SSL structure.
+	mSSL = ssl;
+
+	return B_OK;
+}
+
+/*------------------------------------------------------------------------------*\
+	StopEncryption()
+		-	
+\*------------------------------------------------------------------------------*/
+status_t BmOpenSslNetEndpoint::StopEncryption() {
+	if (!mSSL)
+		return B_BAD_VALUE;
+
+	// shutdown connection
+	if (SSL_shutdown( mSSL) == 0)
+		SSL_shutdown( mSSL);	// as per documentation (bidirectional shutdown)
+
+	// free SSL structure
+	SSL_free( mSSL);
+	mSSL = NULL;
+
+	return B_OK;
+}
+
+/*------------------------------------------------------------------------------*\
+	EncryptionIsActive()
+		-	
+\*------------------------------------------------------------------------------*/
+bool BmOpenSslNetEndpoint::EncryptionIsActive() 
+{
+	return mSSL != NULL;
+}
+
+/*------------------------------------------------------------------------------*\
+	Send()
+		-	
+\*------------------------------------------------------------------------------*/
+int32 BmOpenSslNetEndpoint::Send( const void* buffer, size_t size, int flags) {
+	if (!mSSL)
+		return inherited::Send(buffer, size, flags);
+
+	if (!buffer)
+		return B_BAD_VALUE;
+
+	int result = SSL_write( mSSL, buffer, size);
+	if (result > 0)
+		return result;
+
+	// translate the error
+	return _TranslateErrorCode( result);
+}
+
+/*------------------------------------------------------------------------------*\
+	Receive()
+		-	
+\*------------------------------------------------------------------------------*/
+int32 BmOpenSslNetEndpoint::Receive( void* buffer, size_t size, int flags) {
+	if (!mSSL)
+		return inherited::Receive(buffer, size, flags);
+
+	if (!buffer)
+		return B_BAD_VALUE;
+
+	int result = SSL_read( mSSL, buffer, size);
+	if (result > 0)
+		return result;
+
+	// translate the error
+	return _TranslateErrorCode(result);
+}
+
+/*------------------------------------------------------------------------------*\
+	IsDataPending()
+		-	
+\*------------------------------------------------------------------------------*/
+bool BmOpenSslNetEndpoint::IsDataPending( bigtime_t timeout) {
+	// TODO: We might actually need to do something here. Data pending on
+	// the socket doesn't mean that we could also read something from the
+	// SSL connection.
+	return inherited::IsDataPending( timeout);
+}
+
+/*------------------------------------------------------------------------------*\
+	_StartEncryptionAfterConnecting()
+		-	
+\*------------------------------------------------------------------------------*/
+status_t BmOpenSslNetEndpoint::_StartEncryptionAfterConnecting() {
+	if (mSSL)
+		return B_NOT_ALLOWED;
+
+	status_t error = B_OK;
+	if (mEncryptionType.Length()) {
+		error = StartEncryption( mEncryptionType.String());
+		if (error != B_OK)
+			inherited::Close();
+	}
+	return error;
+}
+
+/*------------------------------------------------------------------------------*\
+	_TranslateErrorCode()
+		-	
+\*------------------------------------------------------------------------------*/
+status_t BmOpenSslNetEndpoint::_TranslateErrorCode( int result) {
+	if (!mSSL)
+		return B_NO_INIT;
+
+	if (result > 0)
+		return B_OK;
+
+	switch (SSL_get_error( mSSL, result)) {
+		case SSL_ERROR_WANT_READ:
+		case SSL_ERROR_WANT_WRITE:
+		case SSL_ERROR_WANT_CONNECT:
+		case SSL_ERROR_WANT_ACCEPT:
+			return B_WOULD_BLOCK;
+
+		default:
+			return B_ERROR;
+	}
+
+	// TODO: Set errno? Will that make Error() return the value we set here?
+}
+
+/*------------------------------------------------------------------------------*\
+	GetEncryptionInfo()
+		-	
+\*------------------------------------------------------------------------------*/
+status_t BmOpenSslNetEndpoint::GetEncryptionInfo(BMessage* encryptionInfo)
+{
+	if (!encryptionInfo)
+		return B_BAD_VALUE;
+	encryptionInfo->MakeEmpty();
+	encryptionInfo->AddBool("supports-encryption", true);
+	encryptionInfo->AddString("type", "TLS");
+	encryptionInfo->AddString("type", "SSL");
+	return B_OK;
+}
+
+/*------------------------------------------------------------------------------*\
+	InstantiateNetEndpoint()
+		-	
+\*------------------------------------------------------------------------------*/
+extern "C" __declspec(dllexport)
+BmNetEndpoint* InstantiateNetEndpoint()
+{
+	return new BmOpenSslNetEndpoint();
+}
+
+/*------------------------------------------------------------------------------*\
+	GetEncryptionInfo()
+		-	
+\*------------------------------------------------------------------------------*/
+extern "C" __declspec(dllexport)
+status_t GetEncryptionInfo(BMessage* encryptionInfo)
+{
+	return BmOpenSslNetEndpoint::GetEncryptionInfo(encryptionInfo);
+}
+
---
+++ src-bmDaemon/src-netendpoint-addons/BmOpenSslNetEndpoint.h
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2005, Ingo Weinhold, bonefish@cs.tu-berlin.de.
+ * Distributed under the terms of the MIT license.
+ *
+ * Changed into an addon and extended somewhat 
+ * by Oliver Tappe, zooey@hirschkaefer.de.
+ */
+
+#ifndef _BmOpenSslNetEndpoint_h
+#define _BmOpenSslNetEndpoint_h
+
+#include "BmString.h"
+
+#include "BmNetEndpoint.h"
+
+// avoid inclusion of <openssl/ssl.h> here
+struct ssl_st;
+
+class BmOpenSslNetEndpoint : public BmNetEndpoint {
+	typedef BmNetEndpoint inherited;
+public:
+	BmOpenSslNetEndpoint();
+	virtual ~BmOpenSslNetEndpoint();
+
+	virtual status_t Connect( const BNetAddress& address);
+ 	virtual void Close();
+
+	virtual int Error() const;
+	virtual BmString ErrorStr() const;
+
+	virtual void SetEncryptionType(const char* encType);
+	virtual status_t StartEncryption(const char* encType);
+	virtual status_t StopEncryption();
+	virtual bool EncryptionIsActive();
+
+	virtual int32 Send( const void* buffer, size_t size, int flags = 0);
+	virtual int32 Receive( void* buffer, size_t size, int flags = 0);
+	virtual bool IsDataPending( bigtime_t timeout = 0);
+
+	static status_t GetEncryptionInfo(BMessage* encryptionInfo);
+
+private:
+	status_t _StartEncryptionAfterConnecting();
+	status_t _TranslateErrorCode( int result);
+
+	ssl_st* mSSL;
+	BmString mEncryptionType;
+	int mError;
+};
+
+#endif
